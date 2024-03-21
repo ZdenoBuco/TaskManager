45118410d2d8253ccb7b94b19ee9a451ac28149d
Zdenko Bučo <zdenko.buco@etyka.cz>
2024-03-21 15:11:32 +0100

    add appUserService and AppUserRegistrationDTO

--- src/main/java/org/taskmanager/controllers/WebController.java
+++ src/main/java/org/taskmanager/controllers/WebController.java
@@ -2,11 +2,13 @@ package org.taskmanager.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("")
 public class WebController {
 
-    @GetMapping("")
+    @GetMapping("/")
     public String homePage() {
         return "homePage.html";
     }
